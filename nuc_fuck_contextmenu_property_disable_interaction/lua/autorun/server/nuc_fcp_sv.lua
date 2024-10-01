fab1c60277b55ffa2841778337e676ddec2620d7
YohSambre <YohSambre@users.noreply.github.com>
2024-10-01 11:43:09 +0200

    Add files via upload

---
+++ nuc_fuck_contextmenu_property_disable_interaction/lua/autorun/server/nuc_fcp_sv.lua
@@ -0,0 +1,13 @@
+--------------------------------------------
+--*/ NUC JOIN THE BATTLE
+--------------------------------------------
+MsgC (Color(255, 255, 255), [[                                                                                                              
+= NUC - Fuck Contextmenu Property loaded!
+]])
+
+--------------------------------------------
+--*/ BASE
+--------------------------------------------
+hook.Add( "CanProperty", "NUC_CONTEXTMENU_PROPERTY", function(ply)
+	return false 
+end)
